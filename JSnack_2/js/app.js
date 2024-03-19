d31b044f699db1aad6da47b9036a26093e3dafad
Helmatron <ema.matro@gmail.com>
2024-03-19 17:47:27 +0100

    JS2 - finito

--- JSnack_2/js/app.js
+++ JSnack_2/js/app.js
@@ -6,4 +6,21 @@ console.log('Hola!')
 // Il software stampa prima la parola più corta, poi la parola più lunga.
 
 
+
 // +++ ANALISI e SVILUPPO +++
+
+
+// CHIEDI prima parola
+const word1 = prompt('inserire prima parola')
+console.log(word1)
+
+// CHIEDI seconda parola
+const word2 = prompt('inserire seconda parola')
+console.log(word2)
+
+// SE la prima parola ha più lettere della seconda, stampala in console
+if (word1 > word2) {
+    console.log(word1)
+} else {
+    console.log(word2)
+}
