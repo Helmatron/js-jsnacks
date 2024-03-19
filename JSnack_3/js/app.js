9eef3c67a502c200bea09e03e0ef05b902bbfa5d
Helmatron <ema.matro@gmail.com>
2024-03-19 17:50:43 +0100

    Add: JS3

---
+++ JSnack_3/js/app.js
@@ -0,0 +1,10 @@
+console.log('Hola!')
+
+// +++ PROBLEMA +++
+
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+
+// +++ ANALISI e SVILUPPO +++
+
