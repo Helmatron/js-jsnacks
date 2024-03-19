18995e6a17613eec0d7ef10bf6a340f80fd54677
Helmatron <ema.matro@gmail.com>
2024-03-19 17:39:11 +0100

    Riordino folder

---
+++ JSnack_2/js/app.js
@@ -0,0 +1,30 @@
+// console.log('Hola!')
+
+// +++ PROBLEMA +++
+
+// L’utente inserisce due numeri in successione, con due prompt.
+// Il software stampa il maggiore.
+
+
+// +++ ANALISI e SVILUPPO +++
+
+// Chiedi primo numero
+const num1 = prompt('inserire primo numero')
+// console.log(num1)
+
+// chiedi secondo numero
+const num2 = prompt('inserire secondo numero')
+// console.log(num2)
+
+
+
+// SE il primo numero è maggiore del secondo
+
+if (num1 > num2) {
+    // ALLORA stampa il primo valore
+    console.log(num1)
+
+    // SE INVECE il secondo numero è maggiore del primo
+}  else { console.log(num2)
+
+}
